96a1c74f205fb8ba359f107211612d55ff13966b
RainBowT <rainbowt0506@gmail.com>
2023-02-27 00:12:25 +0800

    Building the Search Bar in the Top Nav Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import React from "react";
+import {
+  MenuIcon,
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
 
 function Header() {
   return (
     <header>
       {/* Top nav */}
-      <div className="flex item-center bg-amazon_blue p-1 flex-grow py-2">
-        <div className="mt-2 flex item-center flex-grow sm:flex-grow-0">
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        {/* Logo */}
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
             width={150}
@@ -15,6 +21,17 @@ function Header() {
             className="cursor-pointer"
           />
         </div>
+
+        {/* Search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500 ml-5">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <MagnifyingGlassIcon className="h-12 p-4" />
+        </div>
+
+
       </div>
     </header>
   );
