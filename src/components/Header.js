282abdacc45e4af711b27c271e73729a381d8fb8
RainBowT <rainbowt0506@gmail.com>
2023-02-27 00:13:01 +0800

    Building the Right Side of the Top Nav Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,29 @@ function Header() {
           <MagnifyingGlassIcon className="h-12 p-4" />
         </div>
 
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello RainBowT</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
 
+          <div className="link">
+            <p>Return</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+
+          <div className="link relative flex items-center">
+            <span
+              className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full 
+            text-black font-bold"
+            >0</span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="font-extrabold md:text-sm hidden md:inline mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
       </div>
     </header>
   );
