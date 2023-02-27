6a775725942ccf22289b3ddb5cde7dd5abe136ec
RainBowT <rainbowt0506@gmail.com>
2023-02-27 23:02:16 +0800

    Implementing Add & Remove Item Functionality

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,8 +1,10 @@
 import { StarIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React from "react";
-import { Currency } from "react-currency-formatter";
+import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
 import { RESOURCE_DOMAIN } from "../Constant";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
 
 function CheckoutProduct({
   id,
@@ -14,6 +16,27 @@ function CheckoutProduct({
   image,
   hasPrime,
 }) {
+  const dispatch = useDispatch();
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+
+    // Push item into redux
+    dispatch(addToBasket(product));
+  };
+
+  const removeItemFromBasket = () => {
+    // Remove item from redux
+    dispatch(removeFromBasket({ id }));
+  };
   return (
     <div className="grid grid-cols-5">
       <Image
@@ -21,6 +44,7 @@ function CheckoutProduct({
         height={200}
         width={200}
         style={{ objectFit: "contain" }}
+        alt="product"
       />
       {/* Middle */}
       <div className="col-span-3 mx-5">
@@ -43,11 +67,21 @@ function CheckoutProduct({
               loading="lazy"
               className="w-12"
               src={RESOURCE_DOMAIN + "/fdw"}
-              alt=""
+              alt="has Prime"
             />
             <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
           </div>
         )}
+      </div>
+
+      {/* Right add/remove buttons */}
+      <div className="flex flex-col space-y-2 my-auto justify-self-end">
+        <button className="button" onClick={addItemToBasket}>
+          Add to Basket
+        </button>
+        <button className="button" onClick={removeItemFromBasket}>
+          Remove from Basket
+        </button>
       </div>
     </div>
   );
