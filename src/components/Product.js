a75401c8eedfa2caaf72ddf8a92c209e59eb6925
RainBowT <rainbowt0506@gmail.com>
2023-02-27 13:27:19 +0800

    Building the Product Feed Component

--- src/components/Product.js
+++ src/components/Product.js
@@ -19,7 +19,12 @@ function Product({ id, title, price, description, category, image }) {
         {category}
       </p>
 
-      <Image src={image} height={200} width={200} objectFit="contain" />
+      <Image
+        src={image}
+        height={200}
+        width={200}
+        style={{ objectFit: "contain" }}
+      />
 
       <h4 className="my-3">{title}</h4>
 
